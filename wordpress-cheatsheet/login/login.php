6fad6ede1a8b8cb06bfdde1c07cd491352a57596
Sebas Korko <frozedviking@gmail.com>
2021-07-30 01:51:21 -0300

    Example login form WP
    wp cheatsheet for quartex.net

---
+++ wordpress-cheatsheet/login/login.php
@@ -0,0 +1,17 @@
+<?php
+/*
+*
+* Outputs the Wordpress login form
+* https://quartex.net/en
+* https://developer.wordpress.org/reference/functions/wp_login_form/
+*
+*/
+$arguments = array(
+'echo' => true,
+'redirect' => get_permalink( get_the_ID() ),
+'remember' => true,
+'value_remember' => true,
+);
+
+wp_login_form( $arguments );
+?>
