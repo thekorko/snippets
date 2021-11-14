7a8fa760202c4ab4bf1ef5fc1f3235feab6e8607
Sebas Korko <frozedviking@gmail.com>
2021-11-14 20:38:23 -0300

    Convert flac to mp3
    Not fully automated but it works

--- bat-2018/ffmpeg/ffmpeg_mp3.bat
+++ bat-2018/ffmpeg/ffmpeg_mp3.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Siple convert audio from flac to mp3 using ffmpeg
 echo by korko www.quartex.net
+::echo Place in the same folder as ffmpeg.exe and modify it.
 D:\ffmpeg.exe -i D:\input.flac -ab 320k -map_metadata 0 -id3v2_version 3 D:\output.mp3
