6b5f31f0509b77bda54d6d62edb228fa85935df2
Sebas Korko <frozedviking@gmail.com>
2021-11-14 20:08:45 -0300

    wp shortcode examples

--- wordpress-cheatsheet/shortcodes.php
+++ wordpress-cheatsheet/shortcodes.php
@@ -1,3 +1,14 @@
-			<?php //echo do_shortcode('[qtx]' . '$text_to_be_wrapped_in_shortcode '. '[/qtx]') ?>
-			<?php //TODO qtx_cat_list('front'); ?>
-tests
+			<?php
+			/*Do shortcodes from template files*/
+			echo do_shortcode('[qtx]' . '$text_to_be_wrapped_in_shortcode '. '[/qtx]')
+
+
+			/*
+			* html center tag shortcode example
+			* https://developer.wordpress.org/reference/functions/do_shortcode/
+			*/
+			function qtx_center_shortcode($atts, $content = null) {
+				return do_shortcode('<center>'.$content.'</center>');
+			}
+			add_shortcode('center', 'qtx_center_shortcode');
+			?>
