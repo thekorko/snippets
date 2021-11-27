d87298a5c4b22f03ab3588b76d0ef9dec0772fed
Sebas Korko <frozedviking@gmail.com>
2021-11-27 04:57:04 -0300

    example

---
+++ php-scripts/discord-php/discord.php
@@ -0,0 +1,20 @@
+<?php
+
+include "autoload.php";
+
+use Discord\Discord;
+
+$discord = new Discord([
+	'token' => 'bot-token',
+]);
+
+$discord->on('ready', function ($discord) {
+	echo "Bot is ready!", PHP_EOL;
+
+	// Listen for messages.
+	$discord->on('message', function ($message, $discord) {
+		echo "{$message->author->username}: {$message->content}",PHP_EOL;
+	});
+});
+
+$discord->run();
