d5a5e5267ad7bdf42548c8a29168ab6a167b695e
Sebas Korko <frozedviking@gmail.com>
2021-11-27 04:05:08 -0300

    Mostrando un poco de PHP por discord live

---
+++ php-scripts/papyrus/index_nitsu.php
@@ -0,0 +1,22 @@
+<center>
+  <p>Hola Mundo, ¿Nitsuga como te va?</p>
+<img style="width:100px;height:auto;background-color:red;border-radius:7px;border:solid 2px;border-color:blue;" src="https://www.php.net/images/logos/php-logo.svg">
+<style>
+.nitsuga {
+  font-size:20px;
+  color:blue;
+  font-family:Papyrus;
+}
+</style>
+<?php
+$a = 10;
+$b = 12;
+$resultado = $a + $b;
+//echo "<br>El resultado de la suma entre $a y $b es igual a $resultado";
+/*Este archivo es un ehjemplo*/
+if ($resultado>20) {
+  echo '<p class="nitsuga">Nitsuga el archimaestre de las artes del Papyrus</p>';
+}
+
+?>
+</center>
