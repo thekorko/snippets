e5d9f9bcbfa68e9b3944c8d320c1cbd17891a7ab
Sebas Korko <68623797+thekorko@users.noreply.github.com>
2022-07-09 04:08:37 -0300

    test

--- php-scripts/keygen_pares/index.php
+++ php-scripts/keygen_pares/index.php
@@ -1,23 +1,30 @@
 <?php
 $keys = array("R5M8T-?CMHP-P?BWG","BPQNZ-B??BY-AXTZX","M?NYN-?IYYH-ZG?LD","NQBCN-?WY?A-JRQ98");
-foreach ($keys as $key) {
-  $filter_a = array();
-  $filter_b = array();
-  for ($i=0; $i < 10; $i++) {
-    for ($j=9; $j > -1; $j--) {
-      $i_j = "(".$i.",".$j.")";
-      $j_i = "(".$j.",".$i.")";
-      if (!in_array($i_j,$filter_a) && !in_array($j_i,$filter_a)) {
-        array_push($filter_a,$i_j);
-        //echo ($i_j);
-      }
-      if (!in_array($j_i,$filter_b) && !in_array($i_j,$filter_a)) {
-        array_push($filter_b,$j_i);
-        //echo ($j_i);
-      }
 
+foreach ($keys as $key) {
+  echo "The key in question is: $key<br>";
+  $parts_a = array();
+  $parts_b = array();
+  $parts_c = array();
+  $key_parts = explode('-', $key);
+  $total_coincidences = substr_count($key,'?');
+  for ($partN=0; $i < $key_parts $; $partN++) {
+    $part_coincidences = substr_count($part[$partN],'?');
+    if ($part_coincidences>0) {
+      for ($p=0; $p < $part_coincidences; $p++) {
+        for ($i=0; $i < 10; $i++) {
+          for ($j=9; $j > -1; $j--) {
+            echo (str_replace('?'));
+          }
+        }
+      }
+    } else {
+      continue;
     }
-    echo "<br>";
   }
+
+  echo "<br>";
+  $result_key = implode('-', $result_key_parts);
+  echo "$result_key";
 }
 ?>
