8fc48462042549aec61caa5e5ff1f5d8e6f8a0b4
Sebas Korko <68623797+thekorko@users.noreply.github.com>
2022-07-14 00:02:15 -0300

    test

--- php-scripts/keygen_pares/index.php
+++ php-scripts/keygen_pares/index.php
@@ -1,7 +1,7 @@
 <?php
 $keys = array("R5M8T-?CMHP-P?BWG","BPQNZ-B??BY-AXTZX","M?NYN-?IYYH-ZG?LD","NQBCN-?WY?A-JRQ98");
-function run_for($parts_of_key,$partN,$key_parts,$part_coincidences) {
-  if ($part_coincidences!=1) {
+
+function run_for(&$parts_of_key,$partN,$key_parts,$part_coincidences) {
     $test = str_split($key_parts[$partN]);
     for ($letter=0; $letter < count($test) ; $letter++) {
       if ($test[$letter]!='?') {
@@ -13,31 +13,15 @@ function run_for($parts_of_key,$partN,$key_parts,$part_coincidences) {
         $var = implode($temp);
         echo ("<br>".$var);
         array_push($parts_of_key[$partN],$var);
-        if ($i==9) {
-          for ($j=9; $j > -1; $j--) {
-            $temp = $test;
-            $temp[$letter] = str_replace('?',$j,$test[$letter]);
-            $var = implode($temp);
-            echo ("<br>".$var);
-            array_push($parts_of_key[$partN],$var);
-          }
-        }
-      }
-    }
-  } else {
-    for ($i=0; $i < 10; $i++) {
-      $var = str_replace('?',$i,$key_parts[$partN]);
-      echo ("<br>".$var);
-      array_push($parts_of_key[$partN],$var);
-      if ($i==9) {
         for ($j=9; $j > -1; $j--) {
-          $var = str_replace('?',$j,$key_parts[$partN]);
+          $temp = $test;
+          $temp[$letter] = str_replace('?',$j,$test[$letter]);
+          $var = implode($temp);
           echo ("<br>".$var);
           array_push($parts_of_key[$partN],$var);
         }
       }
     }
-  }
 }
 foreach ($keys as $key) {
   echo "<br>The key in question is: $key<br>";
@@ -50,12 +34,15 @@ foreach ($keys as $key) {
   $total_coincidences = substr_count($key,'?');
   for ($partN=0; $partN < count($key_parts); $partN++) {
     $part_coincidences = substr_count($key_parts[$partN],'?');
-    if ($part_coincidences>1) {
+    if ($part_coincidences>0) {
       for ($p=0; $p < $part_coincidences; $p++) {
         run_for($parts_of_key,$partN,$key_parts,$part_coincidences);
+        $x = 0;
+        foreach ($parts_of_key[0] as $key_part) {
+          echo "<br>The resulting key is ".$parts_of_key[0][$x].$parts_of_key[1][$x].$parts_of_key[2][$x]."<br>";
+          $x++;
+        }
       }
-    } elseif($part_coincidences==1) {
-      run_for($parts_of_key,$partN,$key_parts,$part_coincidences);
     } else {
       if (empty($parts_of_key[$partN])) {
         echo "$key_parts[$partN]";
